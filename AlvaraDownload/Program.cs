944eb0d3c18f9f375300fa8b40ebd01a6a937095
Thiago <thiagopiress1234@gmail.com>
2026-03-18 21:30:34 -0300

    Instalado o PlayWright e ClosedXML

--- AlvaraDownload/Program.cs
+++ AlvaraDownload/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("AlvarasBot iniciado!");
+﻿using ClosedXML.Excel;
+
+using var workbook = new XLWorkbook("C:\\Users\\thiag\\Downloads\\AlvasBlumenau.xlsx");
+var ws = workbook.Worksheet(1);
+
+var valor = ws.Cell(1, 1).GetString();
+
+Console.WriteLine(valor);
